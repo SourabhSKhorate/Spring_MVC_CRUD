dd0f4aa2847e592a8a907ce8d9f6dc7366df60dd
SourabhSKhorate <khoratesourabh001@gmail.com>
2023-10-25 09:46:52 +0530

    spring mvc crud

---
+++ src/main/java/com/jsp/studentservice/StudentService.java
@@ -0,0 +1,36 @@
+package com.jsp.studentservice;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.jsp.student.Student;
+import com.jsp.studentdao.StudentDao;
+
+@Component
+public class StudentService {
+
+	@Autowired
+	private StudentDao studentDao;
+
+	public Student saveStudent(Student student) {
+		return studentDao.saveStudent(student);
+
+	}
+
+	public boolean deleteStudentById(int id) {
+		return studentDao.deleteStudentById(id);
+	}
+
+	public Student updateStudentByID(int id, Student student) {
+		Student s=studentDao.getStudentById(id);
+		s.setName(student.getName());
+		s.setEmail(student.getEmail());
+		return studentDao.updateStudentByID(s);
+	}
+
+	public List<Student> getAllStudent() {
+		return studentDao.getAllStudent();
+	}
+}
---
+++ src/main/webapp/home.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>${saved}</h1>
+<h1>Welcome to Home Page</h1>
+<a href="createstudent">Create Student</a> <br> <br>
+<a href="viewAllStudents">View All Students</a>
+
+
+</body>
+</html>
